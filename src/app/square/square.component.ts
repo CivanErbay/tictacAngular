e3700cbacded6fbc7410fcaa17b4708719bc4057
navic1992 <civan.erbay@web.de>
2020-08-30 21:07:40 +0200

    Add Styling

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,16 +4,18 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `
 <button nbButton *ngIf="!value"> {{value}} </button>
+<button nbButton hero status="success" *ngIf="value == 'X'">{{value}}</button>
+<button nbButton hero status="info" *ngIf="value == 'O'">{{value}}</button>
   `,
-  styles: []
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
-//Create Random Number Variable
+/* //Create Random Number Variable
   randomNumber = Math.random();
 //Create a Random Number Variable which gets updated each 0,5 Secs
   constructor() {
     setInterval(() => this.randomNumber= Math.random(), 500 )
-  }
+  } */
 
   @Input() value: 'X' | 'O';
 }
